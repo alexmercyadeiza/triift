d7f5ab86c3aeb6aee0560ed3fef3ebd4d8fccd96
Adeiza <alexmercyadeiza@gmail.com>
2022-09-03 15:14:30 +0100

    messenger integration

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import { Toaster } from 'react-hot-toast';
 import '../styles/globals.css'
+import MessengerCustomerChat from "react-messenger-customer-chat";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
       <Toaster />
+      <MessengerCustomerChat
+        pageId="334278140487721"
+        appId="813855139747675"
+        // htmlRef="<REF_STRING>"
+      />
     </>
   );
 }
